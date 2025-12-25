5ff62147a86fc7834734992429af126868ebe037
AlexKorabel <hellvitiss@gmail.com>
2025-12-25 21:13:14 +0300

    Постепенно меняю проверку температуры

--- utils/temperature.py
+++ utils/temperature.py
@@ -4,7 +4,7 @@ from aiogram import Bot
 from psutil import sensors_temperatures
 
 def get_cpu_temp() -> float:
-    temp = sensors_temperatures().get('cpu_thermal')
+    temp = sensors_temperatures().get('cpu_thermal')[0].current
 
     return temp
 
