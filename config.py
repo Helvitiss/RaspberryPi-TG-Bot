81a8fc3115cdf6d43968d67c4aacb0ad21d46f9b
hellv <hellvitiss@gmail.com>
2026-01-03 16:49:49 +0300

    донастроил конфиг

--- config.py
+++ config.py
@@ -5,7 +5,11 @@ load_dotenv()
 
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ALLOWED_USERS = [662529148]
+ALLOWED_USERS = os.getenv("ALLOWED_USERS").split(',')
 
 
 
+
+
+if __name__ == '__main__':
+    print(ALLOWED_USERS)
