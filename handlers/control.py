5e5f8e462c9e1622e6066802d2d4cd2b4c81b5bf
hellv <hellvitiss@gmail.com>
2026-01-07 02:32:05 +0300

    Заглушка хендлера kill

--- handlers/control.py
+++ handlers/control.py
@@ -2,10 +2,12 @@ import subprocess
 from typing import Callable
 
 from aiogram import Router, F
+from aiogram.filters import Command
 from aiogram.types import Message
 from filters import IsAdminFilter
 
 router = Router()
+
 router.message.filter(IsAdminFilter())
 
 @router.message(F.text == "/reboot")
@@ -13,10 +15,17 @@ async def reboot_handler(message: Message):
 
 
     await message.answer("💻 Перезагрузка системы...")
-    subprocess.run("sudo reboot", shell=True)
+    subprocess.run(["sudo","reboot"], shell=True)
 
 
 @router.message(F.text == "/poweroff")
 async def power_off_handler(message: Message):
     await message.answer("💻 Выключение системы...")
-    subprocess.run("sudo poweroff", shell=True)
+    subprocess.run(['sudo', 'poweroff'], shell=True)
+
+
+@router.message(Command("kill"))
+async def kill_handler(message: Message):
+    parts = message.text.split()
+
+    await message.answer(str(parts))
