7aec72e48ecdf285b2a08d3a7d569a754ca09871
AlexKorabel <hellvitiss@gmail.com>
2026-01-11 17:12:55 +0300

    удалена функция без исполььзования

--- core/config.py
+++ core/config.py
@@ -8,10 +8,3 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ALLOWED_USERS = os.getenv("ALLOWED_USERS").split(',')
 
-
-def warmup_process_cpu():
-    for proc in psutil.process_iter():
-        try:
-            proc.cpu_percent(None)
-        except (psutil.NoSuchProcess, psutil.AccessDenied):
-            pass
